3c24747093e2aa25314b176b10697a3cb4c15fb0
yaroslav-dyn <norman_zp@outlook.com>
2016-10-26 10:46:42 +0300

    add slowly scroll

--- js/user.js
+++ js/user.js
@@ -151,6 +151,13 @@ $(document).ready(function() {
 		}
 	}]);
 
+	//slow scroll on  target
+	$('a[href^="#"]').click(function(){
+		var target = $(this).attr('href');
+		$('html, body').animate({scrollTop: $(target).offset().top}, 800);
+		return false;
+	});
+
 
 });
 
