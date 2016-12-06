78953099c6dc241f0fccfd4f8d6891a96da29ca0
yaroslav-dyn <norman_zp@outlook.com>
2016-12-06 14:39:10 +0200

    fix many points

--- ecology/js/parts/app-function.js
+++ ecology/js/parts/app-function.js
@@ -1,72 +1,4 @@
 
-/*
-include parts like: //= part.js
-*/
-
-$(document).ready(function(){
-
-
-      function clearCurrentNav(){
-          $('.menu li').removeClass("current");
-      }
-
-      $(".menu li").on("click", function () {
-          clearCurrentNav();
-          $(this).addClass("current");
-
-      });
-
-
-      $(document).on("click",".top_menu li", function(){
-        clearCurrentNav();
-        var targetData = $(this).attr("target");
-        $(".menu li[target=" + targetData +"]").addClass("current");
-
-      });
-
-
-
-
-      $(document).on("click",".current-post", function(){
-
-          clearCurrentNav();
-          $(".menu li[target=news]").addClass("current");
-
-      });
-
-
-      function windowTop(){
-        $(window).scrollTop(0);
-      }
-
-    $(document).on("click",".referral-a", windowTop);
-
-    // *function for checking elem. top position
-    function sectionPosition(elPos) {
-        var sPos = elPos;
-        return sPos.offset().top;
-    }
-
-    $(window).on('scroll',function(){
-        var docStop = $(this).scrollTop(),
-            fixMan = sectionPosition($('.header-inner'));
-
-         if(fixMan === 0){
-            $('.header-inner').removeClass("fixed");
-            $('.logo').css("display","block");
-
-        }
-        else if(fixMan >= docStop  ){
-            $('.header-inner').addClass("fixed");
-            $('.logo').css("display","none");
-        }
-
-    });
-
-
-
-});//end ready
-
 
 var app = angular.module("eco-app", ["ngRoute"]);
 
@@ -154,3 +86,4 @@ var app = angular.module("eco-app", ["ngRoute"]);
             })
 
     });
+
---
+++ ecology/js/parts/functions.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+
+
+      function clearCurrentNav(){
+          $('.menu li').removeClass("current");
+      }
+
+      $(".menu li").on("click", function () {
+          clearCurrentNav();
+          $(this).addClass("current");
+
+      });
+
+
+      $(document).on("click",".top_menu li", function(){
+        clearCurrentNav();
+        var targetData = $(this).attr("target");
+        $(".menu li[target=" + targetData +"]").addClass("current");
+
+      });
+
+
+
+
+      $(document).on("click",".current-post", function(){
+
+          clearCurrentNav();
+          $(".menu li[target=news]").addClass("current");
+
+      });
+
+
+      function windowTop(){
+        $(window).scrollTop(0);
+      }
+
+    $(document).on("click",".referral-a", windowTop);
+
+    // *function for checking elem. top position
+    function sectionPosition(elPos) {
+        var sPos = elPos;
+        return sPos.offset().top;
+    }
+
+    $(window).on('scroll',function(){
+        var docStop = $(this).scrollTop(),
+            fixMan = sectionPosition($('.header-inner'));
+
+         if(fixMan === 0){
+            $('.header-inner').removeClass("fixed");
+            $('.logo').css("display","block");
+
+        }
+        else if(fixMan >= docStop  ){
+            $('.header-inner').addClass("fixed");
+            $('.logo').css("display","none");
+        }
+
+    });
+
+
+
+});//end ready
+
